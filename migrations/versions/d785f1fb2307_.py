25b7170d489bc278ac61ea04ff81400c19d54cb0
hgy59 <hpgysin@gmail.com>
2018-04-03 20:29:18 +0200

    db migration script updated include migration of screenshot path column (see 2af5e5f668fd8dbf5ebdeff208076c3380077b44)

---
+++ migrations/versions/d785f1fb2307_.py
@@ -0,0 +1,31 @@
+"""Update for DSM6, screenshot path adjusted for longer package names
+
+Revision ID: d785f1fb2307
+Revises: 26b4c36c11e
+Create Date: 2018-04-03 20:18:47.053636
+
+"""
+revision = 'd785f1fb2307'
+down_revision = '26b4c36c11e'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+
+def upgrade():
+    op.alter_column('screenshot', 'path',
+               existing_type=sa.VARCHAR(length=100),
+               type_=sa.Unicode(length=200),
+               existing_nullable=False)
+    op.add_column('version', sa.Column('conf_privilege', sa.Unicode(length=255), nullable=True))
+    op.add_column('version', sa.Column('conf_resource', sa.Unicode(length=255), nullable=True))
+
+
+def downgrade():
+    op.drop_column('version', 'conf_resource')
+    op.drop_column('version', 'conf_privilege')
+    op.alter_column('screenshot', 'path',
+               existing_type=sa.Unicode(length=200),
+               type_=sa.VARCHAR(length=100),
+               existing_nullable=False)
