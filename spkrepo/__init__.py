184d892514c4dcdf2af56a70668ee90f6494ea79
Michael Reid <943378+mreid-tt@users.noreply.github.com>
2026-06-29 05:56:09 -0400

    bump version to 0.6.9 (#280)

--- spkrepo/__init__.py
+++ spkrepo/__init__.py
@@ -1,11 +1,4 @@
 # -*- coding: utf-8 -*-
-__title__ = "spkrepo"
-__description__ = "Synology Package Repository"
-__version__ = "0.1"
-__author__ = "Antoine Bertin"
-__license__ = "MIT"
-__copyright__ = "Copyright 2014 Antoine Bertin"
-
 from .app import create_app
 
 __all__ = ["create_app"]
